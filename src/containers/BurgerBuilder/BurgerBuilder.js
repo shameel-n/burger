c1c6585420b6cbe7b405866be0982c77aa4f16b0
MUSHAM-BLRM20 <muhammed.shameel@eagleview.com>
2020-10-07 08:49:47 +0530

    first commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Aux from "../../hoc/Aux";
+
+class BurgerBuilder extends React.Component {
+  render() {
+    return <Aux>Burger</Aux>;
+  }
+}
+
+export default BurgerBuilder;
